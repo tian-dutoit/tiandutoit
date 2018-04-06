2dbc02398cf435d2c2c0726044ed2a7fd0244443
tian-dutoit <tiandutoit.nz@gmail.com>
2018-04-06 13:50:47 +1200

    setting up boilerplate

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 
-const App = props => {
-  const circle = {
-    cx: props.width / 2,
-    cy: props.height / 2,
-    level: 0,
-    r: 256
-  }
-
+const App = function () {
   return (
-    <svg width={props.width} height={props.height}>
-      <circle cx={circle.cx} cy={circle.cy} r={circle.r} />
-    </svg>
+    <div>
+      <p>Test</p>
+
+    </div>
   )
 }
 
