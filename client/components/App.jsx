1d595df7877e5d5e0a5580a7271143bf92ca8605
tian-dutoit <tiandutoit.nz@gmail.com>
2018-04-06 14:38:51 +1200

    images added for projects

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const App = function () {
   return (
-    <div>
-      <p>Test</p>
-
+    <div className = 'container'>
+      <div className = 'profile'>
+        <p>Title</p>
+        <img className = 'profileImage' src = 'images/profile.jpg' />
+      </div>
+      <div className = 'project'>
+        <p>Marama</p>
+        <img className = 'projectImage' src = 'images/marama.png' />
+      </div>
+      <div className = 'project'>
+        <p>Super</p>
+        <img className = 'projectImage' src = 'images/super.png' />
+      </div>
+      <div className = 'project'>
+        <p>Bomb Defuser</p>
+        <img className = 'projectImage' src = 'images/bomb.png' />
+      </div>
     </div>
   )
 }
